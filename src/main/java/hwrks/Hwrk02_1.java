675f49d49ae95d26c7e66ed05e8ff2092805e111
Solea <cassandra01@yandex.ru>
2023-01-29 15:12:39 +0300

    дз №2

--- src/main/java/hwrks/Hwrk02_1.java
+++ src/main/java/hwrks/Hwrk02_1.java
@@ -6,7 +6,7 @@ import static hwrks.Stuff_hwrk.readStr;
 public class Hwrk02_1 {
     public static void main(String[] args) {
         System.out.print("Введите строку: ");
-        char[] inputStr=readStr().
+        char[] inputStr = readStr().
                 toLowerCase().
                 toCharArray();
         if (isPalindrome(inputStr)) {
@@ -18,9 +18,10 @@ public class Hwrk02_1 {
 
     private static boolean isPalindrome(char[] tempstr) {
         boolean flag = true;
-        for (int i = 0; i < (tempstr.length-1)/2; i++) {
-            if (tempstr[i] != tempstr[tempstr.length-1-i]) {
-                flag=false;
+        for (int i = 0; i < (tempstr.length - 1) / 2; i++) {
+            if (tempstr[i] != tempstr[tempstr.length - 1 - i]) {
+                flag = false;
+                break; //я протестую против этого решения. а идея говорит, что так надо. кто прав?
             }
         }
         return flag;
