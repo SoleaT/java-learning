21df3c409574b22e68e13a6921121a2e38ba01f7
Solea <cassandra01@yandex.ru>
2023-02-02 16:39:33 +0300

    дз№4

--- src/main/java/hwrks/Hwrk04_2.java
+++ src/main/java/hwrks/Hwrk04_2.java
@@ -7,12 +7,16 @@ public class Hwrk04_2 {
     public static void main(String[] args) {
         LinkedList<String> workArray = new LinkedList<>(List.of("a1", "b2", "c3", "d4", "e5", "f6", "g7"));
         System.out.println(workArray);
+        reverseLL(workArray);
+        System.out.println(workArray);
+    }
+
+    private static void reverseLL(LinkedList<String> workArray) {
         String temp;
         for (int i = 0; i < workArray.size()-1; i++) {
             temp = workArray.getLast();
             workArray.add(i, temp);
             workArray.removeLast();
         }
-        System.out.println(workArray);
     }
 }
